b47c477422187764991fc7a89b05328884a20a51
relbns <ariel@stoketalent.com>
2025-05-17 23:43:57 +0300

    remove supabase files

--- supabase/migrations/20250509230800_make_scoped_path_repo_path_nullable.sql
+++
@@ -1,3 +0,0 @@
--- Make the path_in_repo column nullable in the scoped_paths table
-ALTER TABLE scoped_paths
-ALTER COLUMN path_in_repo DROP NOT NULL;
--- supabase/migrations/20250509231100_make_scoped_path_name_nullable.sql
+++
@@ -1,3 +0,0 @@
--- Make the name column nullable in the scoped_paths table
-ALTER TABLE scoped_paths
-ALTER COLUMN name DROP NOT NULL;
